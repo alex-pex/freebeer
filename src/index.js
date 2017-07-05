5f14da0194027bb327e620f4600d3ad76e611afb
alex-pex <alexandre.paixao@stadline.com>
2017-07-05 13:43:13 +0200

    Easier root component import

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "app/components/App";
+import App from "app";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
 
 if (module.hot) {
-  module.hot.accept("app/components/App", () => {
+  module.hot.accept("app", () => {
     ReactDOM.render(<App />, document.getElementById("root"));
   });
 }
