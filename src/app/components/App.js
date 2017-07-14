a9d9abf1c0f290e575beecc76fa8db1a98e4b41b
alex-pex <alexandre.paixao@stadline.com>
2017-07-14 18:05:39 +0200

    Initial design

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -1,17 +1,34 @@
 import React from "react";
+import { Card, CardTitle, CardText, CardActions, Button, Toolbar, List, ListItem, FontIcon } from "react-md";
 
-import logo from "./logo.svg";
 import "./App.css";
 
+const beerIcon = <FontIcon iconClassName="fa fa-beer" />;
+
 const App = () =>
-  <div className="App">
-    <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>Welcome to React</h2>
+  <div className="toolbar-group">
+    <Toolbar colored title="Dude, you owe me a beer!" />
+    <div className="md-grid">
+      <Card className="md-cell">
+        <CardTitle title="@alex-pex" />
+        <List ordered className="weather-list">
+          <ListItem primaryText="Tuesday" leftIcon={beerIcon} />
+          <ListItem primaryText="Wednesday" leftIcon={beerIcon} />
+          <ListItem primaryText="Thursday" leftIcon={beerIcon} />
+        </List>
+        <CardActions>
+          <Button raised primary label="Je t'en dois une !" />
+        </CardActions>
+      </Card>
+
+      <Card className="md-cell">
+        <CardTitle title="@waelele" />
+        <CardText>Aucune bière</CardText>
+        <CardActions>
+          <Button raised primary label="Je t'en dois une !" />
+        </CardActions>
+      </Card>
     </div>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
   </div>;
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import App from "app";
 
 import renderApp from "./renderApp";
 import registerServiceWorker from "./registerServiceWorker";
+import "react-md/dist/react-md.green-amber.min.css";
 import "./index.css";
 
 renderApp(<App />, document.getElementById("root"));
